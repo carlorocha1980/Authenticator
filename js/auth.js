cc985578d73f1de9a9936d49e77c7eb8c3ab08ea
carlorocha1980 <carlo.rocha@gmail.com>
2022-03-15 17:27:11 -0300

    develop

--- js/auth.js
+++ js/auth.js
@@ -1,21 +1,19 @@
+// Como traduzir no código as informações de autenticação do Firebase, permite uso de multilanguage.
+firebase.auth().languageCode = "pt-BR"
 // Função que trata a submissão do formulário de autenticação
 authForm.onsubmit = function(event) {
   showItem(loading) /*esta função será apresentada sempre que for necessário apresentar o icon Loading*/
   event.preventDefault()
   if (authForm.submitAuthForm.innerHTML == 'Acessar') {
-      firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value)/*.then(function (user) {
-          console.log('Login efetuado com sucesso')
-          console.log(user)
-      })*/.catch(function (error){
-          console.log('Falha no acesso!')
-          console.log(error)
+      firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error){
+            showError('Falha no acesso:' , error)
       })
   } else {
           firebase.auth().createUserWithEmailAndPassword(authForm.email.value, authForm.password.value).then(function (user) {
-              console.log('Usuário cadastrado com sucesso!')
-              console.log(user)
+            alert ('Usuário cadastrado com Sucesso!')  
+            console.log(user)
           }).catch(function (error){
-              console.log('Falha no Cadastro!')
+              showError('Falha no Cadastro:' , error)
               console.log(error)
           })
   }
@@ -25,24 +23,131 @@ authForm.onsubmit = function(event) {
 Função para validar o status do usuário, utilizando um função anônima
 A aplicação desta função permite a retirada do .then do User ".then(function (user)" para efeito de estudo vira comentário */
 // função que centraliza e trata a autenticação
-firebase.auth().onAuthStateChanged(function (user) {
-  hideItem(loading)
-  if (user) {
-      showUserContent(user)
-      console.log('Usuário Autenticado')
-      console.log(user)
-  }
-  else {
-      showAuth()
-      console.log('Usuário não não atenticado')
-  }
-})
+firebase.auth().onAuthStateChanged((user) => {
+        hideItem(loading)
+        if (user) {
+            showUserContent(user)
+            console.log('Usuário Autenticado')
+            console.log(user)
+        }
+        else {
+            showAuth()
+            console.log('Usuário não não atenticado')
+        }
+    })
 
 //Função que permite ao usuário deslogar do App, catch importante para captura erros
 function signOut() {
   firebase.auth().signOut().catch(function(error) {
+      showError('Falha no logout:' , error)
+      console.log(user)
       console.log('Não foi possivel deslogar!')
       console.log(error)
+  }).finally(function (){
+      hideItem(loading)
   })
 }
   
+//Função que permite a validação do email do usuário
+function sendEmailVerification() {
+    var user = firebase.auth().currentUser
+    showItem(loading)
+    user.sendEmailVerification(actionCodeSettings).then(function() {
+        alert(`Email de verificação enviado para, ${user.email}, verifique sua caixa de spam!`)
+        console.log(sendEmailVerification)
+    }).catch(function(erro){
+        showError('Erro ao enviar o email de verificação!' , error)
+    }).finally(function (){
+        hideItem(loading)
+    })
+}
+
+//Função que permite reset de senha
+function sendPasswordResetEmail() {
+    var email = prompt('Insira seu email para redefinir sua senha!', authForm.email.value)
+    if (email) {
+        showItem(loading)
+        firebase.auth().sendPasswordResetEmail(email, actionCodeSettings).then(function () {
+            alert(`Email de redefinição de senha enviado para, ${email}, verifique sua caixa de spam!`)
+        }).catch(function(error) {
+            showError('Erro ao enviar o email redefinição de senha!' , error)
+            //alert('Erro ao enviar o email redefinição de senha!')
+            console.log(error)
+        }).finally(function () {
+            hideItem(loading)
+            })
+    }else {
+        alert('É necessário preencher o campo email redefinição de senha!')
+    }
+}
+
+//Função autenticação com Google
+function signInWithGoogle() {
+    showItem(loading)
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(function(error){
+        alert('Erro no login com Google Account')
+        console.log(error)
+        hideItem(loading)
+        showError('Falha no login com Google Account' , error)
+    })
+}
+
+//Função autenticação com GitHub
+function signInWithGithub() {
+    showItem(loading)
+    firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider()).catch(function(error){
+        alert('Erro no login com GitHub Account')
+        console.log(error)
+        hideItem(loading)
+        showError('Erro ao enviar o email redefinição de senha!' , error)
+    })
+}
+
+//Função autenticação com Facebook
+function signInWithFacebook() {
+    showItem(loading)
+    firebase.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider()).catch(function(error){
+        alert('Erro no login com Facebook')
+        console.log(error)
+        hideItem(loading)
+        showError('Erro ao enviar o email redefinição de senha!' , error)
+    })
+}
+
+//Função para alterar user information
+function updateUserInfo() {
+    var user = firebase.auth().currentUser
+    var newUserName = prompt('Informe um novo nome de usuário', userName.innerHTML)
+    if (newUserName) {
+        userName.innerHTML = newUserName
+        firebase.auth().currentUser.updateProfile({
+            displayName: newUserName
+        }).catch(function(error) {
+            alert('Houve um erro na atualização do nome')
+            console.log(error)
+            hideItem(loading)
+            showError('Erro ao enviar o email redefinição de senha!' , error)
+        })
+    }else {
+        alert('O nome do usuário não pode estar vazio!')
+        console.log(error)
+    }
+}
+
+//Função para excluir conta de usuário da base de dados do Firebase
+function deleteUserAccount() {
+    showItem(loading)
+    var user = firebase.auth().currentUser
+    var confirmation = confirm('Você realmente deseja excluir a sua conta? Seu histórico será perdido!')
+    var secondConfirmation = confirm('Você tem certeza? Esta ação não poderá ser desfeita!')
+    if (secondConfirmation) {
+        firebase.auth().currentUser.delete().then(function () {
+            alert('Conta excluída com sucesso!')
+        }).finally(function() {
+            hideItem(loading)
+        })
+    }else {
+        alert('Não foi possível excluir seu usuário!')
+        hideItem(loading)
+    }
+}
--- js/utils.js
+++ js/utils.js
@@ -3,29 +3,33 @@ var authForm = document.getElementById('authForm')
 var authFormTitle = document.getElementById('authFormTitle')
 var register = document.getElementById('register')
 var access = document.getElementById('access')
-
 var loading = document.getElementById('loading')
-
 var auth = document.getElementById('auth')
 var userContent = document.getElementById('userContent')
-
 var userEmail = document.getElementById('userEmail')
-
+var emailVerified = document.getElementById('emailVerified')
+var sendEmailVerificationDiv = document.getElementById('sendEmailVerificationDiv')
+var passwordReset = document.getElementById('passwordReset')
+var userName = document.getElementById('userName')
+var userImg = document.getElementById('userImg')
 
 // Alterar o formulário de autenticação para o cadastro de novas contas
 function toggleToRegister() {
   authForm.submitAuthForm.innerHTML = 'Criar conta'
   authFormTitle.innerHTML = 'Faça seu cadastro'
-  hideItem(register)
-  showItem(access)
+  hideItem(register) //hide atalho para casdastrar contar
+  showItem(access) //show atalho para casdastrar contar
+  hideItem(passwordReset) //hide na tela de registor
 }
 
 // Alterar o formulário de autenticação para o acesso de contas já existentes
 function toggleToAccess() {
   authForm.submitAuthForm.innerHTML = 'Acessar'
   authFormTitle.innerHTML = 'Acesse sua conta para continuar!'
-  hideItem(access)
+  hideItem(access) 
   showItem(register)
+  showItem(passwordReset) //show na tela de login.
+  hideItem(loading)
 }
 
 // Simpplifica a exibição de elementos da página
@@ -39,6 +43,23 @@ function hideItem(element) {
 
 //Função destinada a apresenta a tela de usuário autenticados. Necessário desenvolver área logada
 function showUserContent(user) {
+  console.log(user)
+  if(user.providerData[0].providerId != 'password'){
+    emailVerified.innerHTML = 'Verificado por provador Confiável'
+    hideItem(sendEmailVerificationDiv)
+    } else{
+      if(user.emailVerified) {
+        emailVerified.innerHTML == 'e-mail Verificado'
+        hideItem(sendEmailVerificationDiv)
+      } else{
+        emailVerified.innerHTML == 'e-mail não Verificado'
+        showItem(sendEmailVerificationDiv)
+      }
+  }
+  
+  //Utilizando dados do Google Authentication
+  userImg.src = user.photoURL ? user.photoURL : 'https://firebasestorage.googleapis.com/v0/b/studio-oz.appspot.com/o/public%2Fimg%2FunknownUser.png?alt=media&token=4a58a4ac-0845-4351-b3e3-2eafdb5d477c'
+  userName.innerHTML = user.displayName ? user.displayName : 'Usuário Visitante'
   userEmail.innerHTML = user.email
   hideItem(auth)
   showItem(userContent)
@@ -51,3 +72,30 @@ function showAuth() {
   hideItem(userContent)
   showItem(auth)
 }
+
+//Central de tratamento de Erros para o Firebase
+function showError(prefix, error) {
+  console.log(error.code)
+  hideItem(loading)
+
+  switch (error.code) {
+    case 'auth/invalid-email': alert(prefix + ' ' + 'Este não é um email válido!')
+    break;
+    case 'auth/email-already-in-use': alert(prefix  + ' ' + 'Este email já possui um cadastro!')
+    break;
+    case 'auth/weak-password': alert(prefix + ' ' + 'A senha precisa ter pelo menos 6 caracteres!')
+    break;
+    case 'auth/wrong-password': alert(prefix + ' ' + ' Senha ou Usuário inválido!')
+    break;
+    case 'auth/too-many-requests': alert(prefix + ' ' + ' Usuário está tentando logar várias vezes seguidas, por questão de segurança aguarde 1 min e tente novamente!')
+    break;
+    default: alert(prefix + ' ' + error.message)
+  }
+}
+
+//Atributos extras para melhoria da experiência do cliente.
+var actionCodeSettings = {
+  url: 'https://studio-oz.web.app/'
+}
+
+//Função para atualizar foto do usuário
